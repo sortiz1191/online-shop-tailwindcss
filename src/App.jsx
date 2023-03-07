fce2bf11d2250c165ec0ad2ea489808c6721bf3c
sortizowlh <sortiz@owlh.net>
2023-03-07 20:24:58 +0100

    Add main menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Sidebar from './components/shared/Sidebar'
 import { RiMenu3Fill, RiUser3Line, RiPieChartLine, RiAddLine, RiCloseLine, RiSearch2Line, RiArrowDownSLine} from "react-icons/ri";
-
+import dish from './assets/dish.png'
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-		  <Sidebar showMenu={showMenu} />
+      <Sidebar showMenu={showMenu} />
       {/* Mobile menu */}
       <nav className="bg-[#1F1D2B] lg:hidden fixed w-full items-center text-gray-400
         justify-between py-4 px-8 bottom-0 text-3xl flex rounded-tr-xl rounded-tl-xl">
@@ -31,15 +31,15 @@ function App() {
           {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
         </button>
       </nav>
-      <main className='lg:pl-28 grid grid-cols-1 lg:grid-cols-8'>
-        <div className='lg:col-span-6'>
+      <main className='lg:pl-32 grid grid-cols-1 lg:grid-cols-8 p-4 pb-20'>
+        <div className='lg:col-span-6 md:p-8'>
           {/* header */}
-          <header className='p-4'>
+          <header>
             <div className='flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6'>
               {/* title and search */}
               <div>
                 <h1 className='text-2xl text-gray-300'>Jeager Resto</h1>
-                <p className='text-gray-500'>03 Marzo 2023</p>
+                <p className='text-gray-500'>March 03, 2023</p>
               </div>
               {/* search bar */}
               <form >
@@ -54,7 +54,7 @@ function App() {
 
             {/* tabs */}
             <nav className='text-gray-300 flex items-center justify-between border-b
-            md:justify-start md:gap-8'>
+            md:justify-start md:gap-8 mb-8'>
               <a href="" className='relative py-2 pr-4 before:w-1/2 text-[#EC7c6a] before:h-[2px] 
               before:absolute before:bg-[#EC7c6a] before:left-0 before:rounded-full
               before:-bottom-[2px]'
@@ -67,15 +67,59 @@ function App() {
             </nav>
           </header>
           {/* Title content */}
-          <div className='flex items-center justify-between p-4'>
+          <div className='flex items-center justify-between mb-16'>
             <h2 className='text-xl text-gray-300'>Choose dishes</h2>
             <button className='flex items-center gap-4 text-gray-300 bg-[#1f1d2b] 
             py-2 px-4 rounded-lg'>
               <RiArrowDownSLine />Dine in
             </button>
           </div>
+          {/* Content */}
+          <div className='p-8 grid grid-cols-1 md:grid-cols-2 gap-16 2xl:grid-cols-3'>
+            {/* card */}
+            <div className='bg-[#1f1d2b] p-8 px-20 gap-2 rounded-xl flex flex-col items-center text-center text-gray-300'>
+              <img src={dish} alt="Dish" className='w-32 h-32 object-cover -mt-20 shadow-xl rounded-full'/>
+              <p className='text-xl'>Speacy seassoned seafod noodles</p>
+              <span className='text-gray-400'>$2.29</span>
+              <p className='text-gray-600'>20 bowls available</p>
+            </div>
+            <div className='bg-[#1f1d2b] p-8 px-20 gap-2 rounded-xl flex flex-col items-center text-center text-gray-300'>
+              <img src={dish} alt="Dish" className='w-32 h-32 object-cover -mt-20 shadow-xl rounded-full'/>
+              <p className='text-xl'>Speacy seassoned seafod noodles</p>
+              <span className='text-gray-400'>$2.29</span>
+              <p className='text-gray-600'>20 bowls available</p>
+            </div>
+            <div className='bg-[#1f1d2b] p-8 px-20 gap-2 rounded-xl flex flex-col items-center text-center text-gray-300'>
+              <img src={dish} alt="Dish" className='w-32 h-32 object-cover -mt-20 shadow-xl rounded-full'/>
+              <p className='text-xl'>Speacy seassoned seafod noodles</p>
+              <span className='text-gray-400'>$2.29</span>
+              <p className='text-gray-600'>20 bowls available</p>
+            </div>
+            <div className='bg-[#1f1d2b] p-8 px-20 gap-2 rounded-xl flex flex-col items-center text-center text-gray-300'>
+              <img src={dish} alt="Dish" className='w-32 h-32 object-cover -mt-20 shadow-xl rounded-full'/>
+              <p className='text-xl'>Speacy seassoned seafod noodles</p>
+              <span className='text-gray-400'>$2.29</span>
+              <p className='text-gray-600'>20 bowls available</p>
+            </div>
+            <div className='bg-[#1f1d2b] p-8 px-20 gap-2 rounded-xl flex flex-col items-center text-center text-gray-300'>
+              <img src={dish} alt="Dish" className='w-32 h-32 object-cover -mt-20 shadow-xl rounded-full'/>
+              <p className='text-xl'>Speacy seassoned seafod noodles</p>
+              <span className='text-gray-400'>$2.29</span>
+              <p className='text-gray-600'>20 bowls available</p>
+            </div>
+            <div className='bg-[#1f1d2b] p-8 px-20 gap-2 rounded-xl flex flex-col items-center text-center text-gray-300'>
+              <img src={dish} alt="Dish" className='w-32 h-32 object-cover -mt-20 shadow-xl rounded-full'/>
+              <p className='text-xl'>Speacy seassoned seafod noodles</p>
+              <span className='text-gray-400'>$2.29</span>
+              <p className='text-gray-600'>20 bowls available</p>
+            </div>
+          </div>
         </div>
-        <div className='lg:col-span-2 fixed lg:static right-0'>Carrito</div>
+        <div className='w-full h-full lg:col-span-2 lg:static right-0 top-0 bg-[#1f1d2b]'>
+          <div className='relative'>
+            <RiCloseLine className='absolute left-4 top-4 p-4 box-content text-gray-300 bg-[#262837] rounded-full'/>
+          </div>
+        </div>
       </main>
     </div>
   )
